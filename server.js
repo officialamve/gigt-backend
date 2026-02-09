52344d8e42a2950bb7769d57e11f0e8fbfaa4f01
Amal Mathew <amstudio678@gmail.com>
2026-02-09 14:53:49 +0530

    beauty

--- server.js
+++ server.js
@@ -20,3 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
+app.get("/", (req, res) => {
+  res.send("Gigt Backend is running 🚀");
+});
