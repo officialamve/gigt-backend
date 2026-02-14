c215b662a3604ae316ac1a9f67122b0ec388d56e
Amal Mathew <amstudio678@gmail.com>
2026-02-14 21:05:37 +0530

    username backend

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,10 +8,7 @@ const User = require("../models/User");
 router.get("/me", auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id)
-      .select("-password")
-      .populate("posts")
-      .populate("owned")
-      .populate("gifts");
+  .select("-password");
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
